d1a315fd675d86b713baf568f5f86ede2be88aa9
Lehgolaz <lehgolaz66rodrigues@email.com>
2023-08-07 21:32:04 -0300

    agenda

--- resources/views/tipos/index.blade.php
+++ resources/views/tipos/index.blade.php
@@ -1,4 +1,20 @@
-@extends('index')
-@section('content')
-<h1>Lista de Tipos</h1>
+@extends ('index')
+@section('contend')
+<div>
+    <div>
+        <H1>Novo Tipo</H1>
+    </div>
+    <div>
+        <form action="" method="">
+            <div class="mb-3">
+                <label for="descricao" class="form-label">Descrição</label>
+                <input type="text" class="form-control" id="descricao" placeholder="Exemplo: Cancelado" name="descricao">
+            </div>
+            <div class="btn-group">
+                <button type="submit" class="btn btn-success">Salvar</button>
+                <button type="reset" class="btn btn-danger">Limpar</button>
+            </div>
+        </form>
+    </div>
+</div>
 @endsection
