31c3435a4159a6719925f7454ec8f049fa384a4f
Lehgolaz <lehgolaz66rodrigues@email.com>
2023-08-07 21:01:21 -0300

    agenda

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('index');
-});
+})->name('home');
 
 //Get
 Route::get('/tipos', [TipoController::class, 'index'])  ->name('tipos.index');
